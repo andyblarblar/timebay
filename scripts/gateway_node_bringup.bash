08372d3da3393d019578fcf339c7c420b4bf3eeb
Andrew Ealovega <Andrew@Ealovega.dev>
2023-02-27 00:53:05 -0500

    attempt to add bridge back

--- scripts/gateway_node_bringup.bash
+++ scripts/gateway_node_bringup.bash
@@ -12,17 +12,14 @@ iw dev "$device" set meshid timebay
 iw dev "$device" set channel 5 HT40+
 ip link set up "$device"
 
-#TODO remove this line when using bridge
-ip a add 192.168.0.1/24 brd + dev "$device"
-
 # Bridge the mesh and eth
-#ip link add name br0 type bridge
-#ip link set dev br0 up
-#ip link set dev eth0 master br0
-#ip link set dev "$device" master br0
+ip link add name br0 type bridge
+ip link set dev br0 up
+ip link set dev eth0 master br0
+ip link set dev "$device" master br0
 
 # Add bridge ip, which is the only IP assigned to the gateway, accessed via either the mesh or eth
-#ip a add 192.168.0.1/24 brd + dev br0
+ip a add 192.168.0.1/24 brd + dev br0
 
 # Spawn mosquitto in a background job
 mosquitto -c /etc/mosquitto/conf.d/mosquitto.conf &
