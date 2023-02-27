2f1a9770cbf310ba2a413ce536fe5f0e318c1c9d
Andrew Ealovega <Andrew@Ealovega.dev>
2023-02-26 19:13:56 -0500

    Assign an ip only to bridge on gateway

--- scripts/gateway_node_bringup.bash
+++ scripts/gateway_node_bringup.bash
@@ -12,16 +12,14 @@ iw dev "$device" set meshid timebay
 iw dev "$device" set channel 5 HT40+
 ip link set up "$device"
 
-# Set mesh ip
-ip a add 192.168.0.1/24 brd + dev "$device"
-
 # Bridge the mesh and eth
 ip link add name br0 type bridge
 ip link set dev br0 up
 ip link set dev eth0 master br0
 ip link set dev "$device" master br0
 
-# Add bridge ip, to allow for bridge to serve DHCP
-ip a add 192.168.0.2/24 brd + dev br0
+# Add bridge ip, which is the only IP assigned to the gateway, accessed via either the mesh or eth
+ip a add 192.168.0.1/24 brd + dev br0
 
+# bash will yield to stdin, which we dont connect, allowing this docker container to remain running forever
 bash
