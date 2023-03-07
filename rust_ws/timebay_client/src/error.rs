0a55f2ac52dafafd5123e5afb6c18c7bac282cd6
Andrew Ealovega <Andrew@Ealovega.dev>
2023-03-07 16:07:56 -0500

    Remove old client

--- rust_ws/timebay_client/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error(transparent)]
-    MqttConnectionFail(#[from] timebay_common::error::MqttClientError),
-    #[error("MQTT subs are not configured properly")]
-    WrongSub,
-}
