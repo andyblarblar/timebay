4e60dc82cb64cc9be982c74373dc3c7554324278
Andrew Ealovega <Andrew@Ealovega.dev>
2023-04-08 23:35:06 -0400

    Pass node id via arg makes it easier to run in a cronjob, since env vars arnt loaded

--- scripts/run_sensor_node_bat.bash
+++ scripts/run_sensor_node_bat.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Runs on the actual sensor node, starting the docker container automagically on SBC boot. BATMAN edition
+# Node id is passed via arg
 
 # Change to timebay directory if running in cronjob
 cd "$(dirname "$0")/..";
@@ -18,5 +19,11 @@ fi
 # Build docker container if not built yet
 [ -n "$(docker images -q timebay:sensor)" ] || docker build -t timebay:sensor -f sensor-node.dockerfile .
 
+if [ -z "$1" ]
+ then
+     echo "No node id passed!"
+     exit 1
+fi
+
 # Launch sensor node, restarting if it crashed
-docker run --privileged --rm --network host -e NODE_ID="$NODE_ID" -e BROKER_HOST=gateway --add-host=gateway:192.168.0.1 --cap-add SYS_TIME --volume /dev:/dev --entrypoint "/sensor_node_bringup_bat.bash" timebay:sensor
+docker run --privileged --rm --network host -e NODE_ID="$1" -e BROKER_HOST=gateway --add-host=gateway:192.168.0.1 --cap-add SYS_TIME --volume /dev:/dev --entrypoint "/sensor_node_bringup_bat.bash" timebay:sensor
